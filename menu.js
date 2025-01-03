f37852a9eba487813fc4116bc185d305d7571656
Leia Silva <leiabelensilva@gmail.com>
2025-01-03 17:05:58 -0300

    arreglos en funcionamiento del menu

--- menu.js
+++ menu.js
@@ -1,7 +1,14 @@
-const navbar = document.getElementById('navbar')
 const perifl = document.getElementById('perfil')
 const menu = document.getElementById('menu')
+const closeMenu = document.getElementById('closeMenu')
 
 perifl.addEventListener('click', (e) =>{
     menu.classList.toggle('show')
+})
+closeMenu.addEventListener('click', (e) =>{
+    if(menu.classList.contains('show')){
+        menu.classList.remove('show');
+    }else{
+        alert("El menu esta cerrado")
+    }
 })
