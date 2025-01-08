d482cae700e7248ab83abcfc4cab44bb23d6c054
Leia Silva <leiabelensilva@gmail.com>
2025-01-08 18:45:19 -0300

    agregue nuevos estilos de las cards

--- productos.js
+++ productos.js
@@ -1,58 +1,58 @@
 const productos = [
     {
-        id : 1 , 
-        nombre:'MESA EAMES 1.40 X 0.90' , 
-        description:'blanco' , 
-        stock:100 ,
-        precio:145.000 , 
-        codigo:'07110003' ,
-        img : '../assets/07110003.jpg' , 
-    } , 
+        id: 1,
+        nombre: 'MESA EAMES 1.40 X 0.90',
+        description: 'blanco',
+        stock: 100,
+        precio: 145000,
+        codigo: '07110003',
+        img: '../assets/07110003.jpg',
+    },
     {
-        id : 2 , 
-        nombre:'SILLON MESH ALTO- SA 4006 ' , 
-        description:'negro' , 
-        stock:55 ,
-        precio:83.547 , 
-        codigo:'10450013' ,
-        img : '../assets/10450013.jpeg',
-    } , 
+        id: 2,
+        nombre: 'SILLON MESH ALTO- SA 4006 ',
+        description: 'negro',
+        stock: 55,
+        precio: 83.547,
+        codigo: '10450013',
+        img: '../assets/10450013.jpeg',
+    },
     {
-        id : 3 , 
-        nombre:'SILLA EAMES X 4 ' , 
-        description:'blanco' , 
-        stock:57 ,
-        precio:91.116 , 
-        codigo:'07915003' ,
-        img :'../assets/07915003.jpg' ,
-    } , 
+        id: 3,
+        nombre: 'SILLA EAMES X 4 ',
+        description: 'blanco',
+        stock: 57,
+        precio: 91.116,
+        codigo: '07915003',
+        img: '../assets/07915003.jpg',
+    },
     {
-        id : 1 , 
-        nombre:'MESA EAMES 1.40 X 0.90' , 
-        description:'blanco' , 
-        stock:100 ,
-        precio:145.000 , 
-        codigo:'07110003' ,
-        img : '../assets/07110003.jpg', 
-    } , 
+        id: 1,
+        nombre: 'MESA EAMES 1.40 X 0.90',
+        description: 'blanco',
+        stock: 100,
+        precio: 145000,
+        codigo: '07110003',
+        img: '../assets/07110003.jpg',
+    },
     {
-        id : 1 , 
-        nombre:'MESA EAMES 1.40 X 0.90' , 
-        description:'blanco' , 
-        stock:100 ,
-        precio:145.000 , 
-        codigo:'07110003' ,
-        img : '../assets/07110003.jpg', 
-    } , 
+        id: 1,
+        nombre: 'MESA EAMES 1.40 X 0.90',
+        description: 'blanco',
+        stock: 100,
+        precio: 145000,
+        codigo: '07110003',
+        img: '../assets/07110003.jpg',
+    },
     {
-        id : 1 , 
-        nombre:'MESA EAMES 1.40 X 0.90' , 
-        description:'blanco' , 
-        stock:100 ,
-        precio:145.000 , 
-        codigo:'07110003' ,
-        img : '../assets/07110003.jpg', 
-    } , 
+        id: 1,
+        nombre: 'MESA EAMES 1.40 X 0.90',
+        description: 'blanco',
+        stock: 100,
+        precio: 145000,
+        codigo: '07110003',
+        img: '../assets/07110003.jpg',
+    },
 ];
 
 
@@ -60,8 +60,8 @@ const productos = [
 
 const containerProducto = document.getElementById('containerProducto');
 
-function crearCards(productos){
-    productos.forEach( producto =>{
+function crearCards(productos) {
+    productos.forEach(producto => {
         const newCard = document.createElement('div');
         newCard.classList.add('card');
         newCard.innerHTML = `
@@ -71,9 +71,17 @@ function crearCards(productos){
         <div class= "cardContent">
             <h2>${producto.nombre}</h2>
             <h3>${producto.description}</h3>
-            <p>Precio: $${producto.precio}</p>
+            <p>$${producto.precio}</p>
+            <button class= "cardContent-compra"> Agregar al carrito</button>
         </div>
-        <p> Stock : ${producto.stock}
+        <div class= "cardContainer">
+            <div class= "cardContainer-cantidad">
+                <button>-</button>
+                <span> 0 </span>
+                <button>+</button>
+            </div>
+            <p class= "cardContainer-stock"> Stock : ${producto.stock} </p>
+        </div>
         `
 
         containerProducto.appendChild(newCard)
